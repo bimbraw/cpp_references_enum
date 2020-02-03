fb0e7f4d1868e49f5bae279d9197d14c7633a015
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-02-03 14:14:07 -0500

    Added Code

--- cpp_references_enum_1.cpp
+++ cpp_references_enum_1.cpp
@@ -1,1 +1,19 @@
-//Placeholder file
+#include <iostream>
+
+using namespace std;
+
+string name = "blablablabla";
+
+string &blablabla = name;
+string &blabla = name;
+string &bla = name;
+
+int main()
+{
+    cout<< "Value: " << name << " and Address:" << &name << endl;
+    cout<< "Value: " << blablabla << " and Address:" << &blablabla << endl;
+    cout<< "Value: " << blabla << " and Address:" << &blabla << endl;
+    cout<< "Value: " << bla << " and Address:" << &bla << endl;
+    
+    return 0;
+}
